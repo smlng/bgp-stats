ed0adb3d8196db0d1ef680d6cc6ea3334f843759
smlng <s@mlng.net>
2015-05-18 15:44:52 +0200

    update

--- src/R/test_postgres.R
+++ src/R/test_postgres.R
@@ -5,3 +5,5 @@ query_sum_pfxlen <- paste("SELECT masklen(p.prefix), count(masklen(p.prefix)) FR
 query_avg_pfxlen <- paste("SELECT avg(masklen(p.prefix)) FROM",
                           "((SELECT prefix_id FROM t_origins WHERE dataset_id = 1) AS o",
                           "LEFT JOIN t_prefixes p ON o.prefix_id = p.id)",sep=" ")
+
+query_join_origins <- paste(,sep="")
