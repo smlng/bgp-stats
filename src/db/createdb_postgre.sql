8a2bb12fa5d774f4d7df368b585cc5b8db14cfc0
smlng <s@mlng.net>
2015-05-18 17:34:13 +0200

    update

--- src/db/createdb_postgre.sql
+++ src/db/createdb_postgre.sql
@@ -1,13 +1,13 @@
 -- -----------------------
--- table datasets 
+-- table datasets
 -- -----------------------
 DROP TABLE IF EXISTS t_datasets;
 
 CREATE TABLE IF NOT EXISTS t_datasets (
-    id      	SERIAL PRIMARY KEY,
-    ts   		timestamp without time zone NOT NULL,
-    maptype 	TEXT NOT NULL,
-    subtype 	TEXT NOT NULL
+  id          SERIAL PRIMARY KEY,
+  ts          timestamp without time zone NOT NULL,
+  maptype     TEXT NOT NULL,
+  subtype     TEXT NOT NULL
 );
 
 -- -----------------------
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS t_datasets (
 DROP TABLE IF EXISTS t_prefixes;
 
 CREATE TABLE IF NOT EXISTS t_prefixes (
-	id			SERIAL PRIMARY KEY,
-	prefix  	INET NOT NULL
+  id          SERIAL PRIMARY KEY,
+  prefix      INET NOT NULL
 );
 
 -- -----------------------
@@ -26,10 +26,10 @@ CREATE TABLE IF NOT EXISTS t_prefixes (
 DROP TABLE IF EXISTS t_origins;
 
 CREATE TABLE IF NOT EXISTS t_origins (
-	dataset_id	INT,
-	prefix_id  	INT,
-	asn			INT,
-	PRIMARY KEY (dataset_id, prefix_id, asn),
-	FOREIGN KEY (dataset_id) REFERENCES t_datasets (id),
-	FOREIGN KEY (prefix_id) REFERENCES t_prefixes (id)
+  dataset_id  INT,
+  prefix_id   INT,
+  asn         INT,
+  PRIMARY KEY (dataset_id, prefix_id, asn),
+  FOREIGN KEY (dataset_id) REFERENCES t_datasets (id),
+  FOREIGN KEY (prefix_id) REFERENCES t_prefixes (id)
 );
--- src/python/bgp-origin-stats.py
+++ src/python/bgp-origin-stats.py
@@ -12,22 +12,11 @@ import psycopg2
 
 from pymongo import MongoClient
 from datetime import datetime, timedelta
-from multiprocessing import Process, Queue, cpu_count, current_process
-
+from collections import OrderedDict
 verbose = False
 warning = False
 logging = False
 
-re_file_rv = re.compile('rib.(\d+).(\d\d\d\d).bz2')
-re_file_rr = re.compile('bview.(\d+).(\d\d\d\d).gz')
-
-re_path_rv = re.compile('.*/([a-z0-9\.-]+)/bgpdata/\d\d\d\d.\d\d/RIBS.*')
-re_path_rr = re.compile('.*/(rrc\d\d)/\d\d\d\d.\d\d.*')
-
-existing_data = list()
-
-prefix_ids = dict()
-t_file = "/tmp/t_origins.copy"
 def print_log(*objs):
     if logging or verbose:
         print("[LOGS] .", *objs, file=sys.stdout)
@@ -43,42 +32,118 @@ def print_warn(*objs):
 def print_error(*objs):
     print("[ERROR] ", *objs, file=sys.stderr)
 
+def retrieve_postgres(dbconnstr, maxts='2005-01-03', mt='routeviews', st='route-views.wide'):
+    print_info(dbconnstr)
+    try:
+        con = psycopg2.connect(dbconnstr)
+    except Exception, e:
+        print_error("retrieve_postgres: connecting to database")
+        print_error("failed with: %s" % ( e.message))
+        sys.exit(1)
+    cur = con.cursor()
+
+    query_datasets = "SELECT id, ts FROM t_datasets WHERE ts < \'%s\' AND maptype = \'%s\' AND subtype = \'%s\' ORDER BY ts"
+    query_origins = "SELECT p.prefix, o.asn FROM (SELECT * FROM t_origins WHERE dataset_id = \'%s\') AS o LEFT JOIN t_prefixes AS p ON o.prefix_id = p.id"
+
+    datasets = OrderedDict()
+    try:
+        query = query_datasets % (maxts,mt,st)
+        cur.execute(query)
+        rs = cur.fetchall()
+        datasets = OrderedDict((str(rs[i][0]), str(rs[i][1])) for i in range(len(rs)))
+    except Exception, e:
+        print_error("QUERY: %s ; failed with: %s" % (query, e.message))
+        con.rollback()
+
+    results = dict()
+    results['ts'] = list()
+
+    cnt = 0
+    for did in datasets:
+        print_info("RUN %s, processing did: %s, dts: %s" % (str(cnt+1), did, datasets[did]))
+        results['ts'].append(datasets[did])
+        try:
+            query = query_origins % did
+            cur.execute(query)
+            rs = cur.fetchall()
+            for i in range(len(rs)):
+                pfx = str(rs[i][0])
+                asn = int(rs[i][1])
+                if pfx not in results:
+                    results[pfx] = list()
+                    j = 0
+                    while j < cnt:
+                        results[pfx].append([0])
+                        j = j+1
+                if len(results[pfx]) < cnt:
+                    for k in range(len(results[pfx]),cnt):
+                        results[pfx].append([0])
+                if len(results[pfx]) == cnt:
+                    results[pfx].append([asn])
+                else:
+                    results[pfx][cnt].append(asn)
+        except Exception, e:
+            print_error("QUERY: %s ; failed with: %s" % (query, e.message))
+            con.rollback()
+        cnt = cnt+1
+    return results
+
+def process_data(data):
+    results = dict()
+    for pfx in data:
+        # ignore timestamp field
+        if pfx == 'ts':
+            continue
+        #print_info("processing prefix: %s" % (pfx))
+        results[pfx] = list()
+        ts0 = data['ts'][0]
+        as0 = data[pfx][0][0]
+        ts1 = ts0
+        as1 = as0
+        for i in range(2,len(data[pfx])):
+            ts2 = data['ts'][i]
+            as2 = data[pfx][i][0]
+            if as2 != as1:
+                origin_ttl = (as0, ts0, ts1)
+                results[pfx].append( origin_ttl )
+                as0 = as2
+                ts0 = ts2
+                as1 = as2
+                ts1 = ts2
+            else:
+                ts1 = ts2
+    return results
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--logging',      help='Ouptut logging.', action='store_true')
     parser.add_argument('-w', '--warning',      help='Output warnings.', action='store_true')
     parser.add_argument('-v', '--verbose',      help='Verbose output with debug info, logging, and warnings.', action='store_true')
-    parser.add_argument('-t', '--threads',      help='Use threads for parallel and faster processing.', action='store_true', default=False)
-    parser.add_argument('-n', '--numthreads',   help='Set number of threads.', type=int, default=None)
-    imode = parser.add_mutually_exclusive_group(required=True)
-    imode.add_argument('-s', '--single',        help='Process a single file, results are printed to STDOUT.')
-    imode.add_argument('-b', '--bulk',          help='Process a bunch of files in given directory (optional recursive).')
-    parser.add_argument('-r', '--recursive',    help='Search directories recursivly if in bulk mode.', action='store_true')
-    omode = parser.add_mutually_exclusive_group(required=False)
-    omode.add_argument('-j', '--json',          help='Write data to JSON file.',    default=False)
-    omode.add_argument('-c', '--couchdb',       help='Write data to CouchDB.',      default=False)
-    omode.add_argument('-m', '--mongodb',       help='Write data to MongoDB.',      default=False)
-    omode.add_argument('-p', '--postgres',      help='Write data to PostgresqlDB.', default=False)
+    imode = parser.add_mutually_exclusive_group(required=False)
+    imode.add_argument('-j', '--json',          help='Write data to JSON file.',    default=False)
+    imode.add_argument('-c', '--couchdb',       help='Write data to CouchDB.',      default=False)
+    imode.add_argument('-m', '--mongodb',       help='Write data to MongoDB.',      default=False)
+    imode.add_argument('-p', '--postgres',      help='Write data to PostgresqlDB.', default=False)
     args = vars(parser.parse_args())
 
+    # output settings
     global verbose
     verbose   = args['verbose']
-
     global warning
     warning   = args['warning']
-
     global logging
     logging   = args['logging']
 
-    recursive = args['recursive']
-    threads   = args['threads']
-    workers   = args['numthreads']
-    if not workers:
-        workers = cpu_count() / 2
-
+    # run
     start_time = datetime.now()
     print_log("START: " + start_time.strftime('%Y-%m-%d %H:%M:%S'))
 
+    if args['postgres']:
+        res = retrieve_postgres(args['postgres'])
+        out = process_data(res)
+        print(json.dumps(out, sort_keys=True, indent=4, separators=(',', ': ')))
+    else:
+        print_error('No valid data source found!')
     end_time = datetime.now()
     print_log("FINISH: " + end_time.strftime('%Y-%m-%d %H:%M:%S'))
     done_time = end_time - start_time
